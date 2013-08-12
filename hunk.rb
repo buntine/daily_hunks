3311fe69e9489e5f513190c2fac3fb55d25d1600
Andy Buntine <bunts@hhd.com.au>
2013-08-12 15:57:33 +1000

    Ready for live

--- hunk.rb
+++ hunk.rb
@@ -12,7 +12,7 @@ class Hunk
   def treat(width=360, height=550)
     image = Image.read(@file).first
     image.resize_to_fit!(width, height)
-    image = image.posterize(4)
+    image = image.quantize(256, Magick::GRAYColorspace)
     path = File.join(@save_path, "#{Time.now.to_i}#{File.extname(@file)}")
     image.write(path)
 
